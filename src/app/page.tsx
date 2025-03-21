c815207e5e4620162e37bf08fbe724de9a427fdb
Arthur Teixeira <arthur.s.txeira@gmail.com>
2025-03-21 13:10:17 -0300

    feat: create header and hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
-import { ModeToggle } from "@/components/ModeToggle";
+"use client";
+
+import { Header } from "./components/Header";
+import { Hero } from "./components/Hero";
 
 export default function Home() {
   return (
-    <div className="flex h-screen items-center justify-center">
-      <ModeToggle />
+    <div className="relative">
+      <Header />
+      <Hero />
     </div>
   );
 }
