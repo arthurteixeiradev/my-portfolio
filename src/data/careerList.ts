1b55518d56b0559035036e4bac84e40d2f429b07
Arthur Teixeira <arthur.s.txeira@gmail.com>
2025-12-08 09:18:49 -0300

    feat: add vtrina logo

--- src/data/careerList.ts
+++ src/data/careerList.ts
@@ -1,28 +1,29 @@
-import { CareerType } from '@/types/CareerType'
+import { CareerType } from "@/types/CareerType";
+import vtrinaLogo from "@/assets/images/vtrina_logo.jpg";
 
 export const careerList: CareerType[] = [
   {
-    id: '1',
-    title: 'Vtrina',
-    description: 'Software Development Intern',
-    placeholder: 'vtrina.com ->',
+    id: "1",
+    title: "Vtrina",
+    description: "Software Development Intern",
+    placeholder: "vtrina.com ->",
     companyDescription:
-      'Vtrina is a system that allows online stores to control their merchandise, inventory, and data from various sales channels.',
-    icon: 'https://media.licdn.com/dms/image/v2/C4D0BAQFbm6P1lPYIvw/company-logo_200_200/company-logo_200_200/0/1630474413178/vtrina_logo?e=1764201600&v=beta&t=1DGtEEy9aQsGd_bjPU1EYUmjutVT_PzDOfxzap8KwtE',
-    link: 'https://vtrina.com/',
-    startDate: 'Oct 2025',
-    endDate: 'Current',
+      "Vtrina is a system that allows online stores to control their merchandise, inventory, and data from various sales channels.",
+    icon: vtrinaLogo.src,
+    link: "https://vtrina.com/",
+    startDate: "Oct 2025",
+    endDate: "Current",
   },
   {
-    id: '2',
-    title: 'VIFE Solutions',
-    description: 'Frontend Development Intern',
-    placeholder: 'vifesolutions.com ->',
+    id: "2",
+    title: "VIFE Solutions",
+    description: "Frontend Development Intern",
+    placeholder: "vifesolutions.com ->",
     companyDescription:
-      'A software development company that provides services to companies and individuals.',
-    icon: 'https://github.com/vifesolutions.png',
-    link: 'https://vifesolutions.com/',
-    startDate: 'Dec 2024',
-    endDate: 'Oct 2025',
+      "A software development company that provides services to companies and individuals.",
+    icon: "https://github.com/vifesolutions.png",
+    link: "https://vifesolutions.com/",
+    startDate: "Dec 2024",
+    endDate: "Oct 2025",
   },
-]
+];
